c25f6f440a12a6c5f2c698b32ad2f4e74defb3e8
LiuRi <liuri@lemonsoft.net>
2017-01-23 18:00:34 +0800

    临时保存

--- LemonHello4Android/lemonhello/src/main/java/net/lemonsoft/lemonhello/LemonHello.java
+++ LemonHello4Android/lemonhello/src/main/java/net/lemonsoft/lemonhello/LemonHello.java
@@ -15,7 +15,7 @@ import net.lemonsoft.lemonhello.interfaces.LemonPaintContext;
 
 public class LemonHello {
 
-    public static LemonHelloInfo getWarningInfo(String title, String content) {
+    public static LemonHelloInfo getWarningHello(String title, String content) {
         LemonHelloInfo info = new LemonHelloInfo();
         info.setIconLocation(LemonHelloIconLocation.TOP)
                 .setIconPaintContext(new LemonPaintContext() {
@@ -31,9 +31,43 @@ public class LemonHello {
                         paint.setAntiAlias(true);
                         // 绘制外侧的完整浅色圆形
                         canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, (canvas.getWidth() / 2 - 4) * playProgress, paint);
+                        paint.setColor(Color.WHITE);
+                        Path path = new Path();
+                        path.moveTo(canvas.getWidth() * 0.45f, canvas.getHeight() * 0.2f);
+                        path.lineTo(canvas.getWidth() * 0.55f, canvas.getHeight() * 0.2f);
+                        path.lineTo(canvas.getWidth() * (0.55f - 0.025f * playProgress), canvas.getHeight() * (0.2f + 0.45f * playProgress));
+                        path.lineTo(canvas.getWidth() * (0.45f + 0.025f * playProgress), canvas.getHeight() * (0.2f + 0.45f * playProgress));
+                        path.close();
 
+                        path.addRect(canvas.getWidth() * (0.5f - 0.033f * playProgress),
+                                canvas.getWidth() * (0.75f - 0.033f * playProgress),
+                                canvas.getWidth() * (0.5f + 0.033f * playProgress),
+                                canvas.getWidth() * (0.75f + 0.033f * playProgress), Path.Direction.CW);
+                        canvas.drawPath(path, paint);
+                    }
+                })
+                .setIconWidth(60)
+                .setTitle(title)
+                .setContent(content);
+        return info;
+    }
 
-//                                paint.setStyle(Paint.Style.STROKE);
+    public static LemonHelloInfo getInformationHello(String title, String content) {
+        LemonHelloInfo info = new LemonHelloInfo();
+        info.setIconLocation(LemonHelloIconLocation.TOP)
+                .setIconPaintContext(new LemonPaintContext() {
+                    @Override
+                    public void paint(Canvas canvas, float playProgress) {
+                        int color = Color.argb(140, 255, 111, 2);
+                        Paint paint = new Paint();
+                        paint.setStyle(Paint.Style.FILL);
+                        // 设置外侧的浅色圆形为图标颜色的0.1倍透明度
+                        paint.setColor(color);
+                        paint.setStrokeCap(Paint.Cap.ROUND);
+                        paint.setStrokeWidth(8);
+                        paint.setAntiAlias(true);
+                        // 绘制外侧的完整浅色圆形
+                        canvas.drawCircle(canvas.getWidth() / 2, canvas.getHeight() / 2, (canvas.getWidth() / 2 - 4) * playProgress, paint);
                         paint.setColor(Color.WHITE);
                         Path path = new Path();
                         path.moveTo(canvas.getWidth() * 0.45f, canvas.getHeight() * 0.2f);
