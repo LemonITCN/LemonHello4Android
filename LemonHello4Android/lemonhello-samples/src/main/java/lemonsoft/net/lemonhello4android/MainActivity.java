881198fb973825c2f90969f9079e2d543df45dff
LiuRi <liuri@lemonsoft.net>
2017-01-24 10:49:35 +0800

    demo基本完成

--- LemonHello4Android/lemonhello-samples/src/main/java/lemonsoft/net/lemonhello4android/MainActivity.java
+++ LemonHello4Android/lemonhello-samples/src/main/java/lemonsoft/net/lemonhello4android/MainActivity.java
@@ -14,6 +14,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import net.lemonsoft.lemonbubble.LemonBubble;
+import net.lemonsoft.lemonbubble.enums.LemonBubbleLayoutStyle;
+import net.lemonsoft.lemonbubble.enums.LemonBubbleLocationStyle;
 import net.lemonsoft.lemonhello.LemonHello;
 import net.lemonsoft.lemonhello.LemonHelloAction;
 import net.lemonsoft.lemonhello.LemonHelloInfo;
@@ -34,6 +37,7 @@ public class MainActivity extends Activity {
     private LinearLayout btn_warning;
     private LinearLayout btn_information;
     private LinearLayout btn_bookmark;
+    private LinearLayout btn_multiMessages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +49,7 @@ public class MainActivity extends Activity {
         btn_warning = (LinearLayout) findViewById(R.id.btn_warning);
         btn_information = (LinearLayout) findViewById(R.id.btn_information);
         btn_bookmark = (LinearLayout) findViewById(R.id.btn_bookmark);
+        btn_multiMessages = (LinearLayout) findViewById(R.id.btn_multiMessages);
 
         initFunctions();
 
@@ -102,6 +107,151 @@ public class MainActivity extends Activity {
             }
         });
 
+        btn_warning.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LemonHello.getWarningHello("您确认删除这条数据吗？", "删除这条数据后会同时删除其关联的数据，并且无法撤销！")
+                        .addAction(new LemonHelloAction("取消", new LemonHelloActionDelegate() {
+                            @Override
+                            public void onClick(LemonHelloView helloView, LemonHelloInfo helloInfo, LemonHelloAction helloAction) {
+                                helloView.hide();
+                            }
+                        }))
+                        .addAction(new LemonHelloAction("确定删除", Color.RED, new LemonHelloActionDelegate() {
+                            @Override
+                            public void onClick(LemonHelloView helloView, LemonHelloInfo helloInfo, LemonHelloAction helloAction) {
+                                helloView.hide();
+
+                                // 提示框使用了LemonBubble，请您参考：https://github.com/1em0nsOft/LemonBubble4Android
+                                LemonBubble.showRoundProgress(MainActivity.this, "正在删除中...");
+                                new Handler().postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        LemonBubble.showRight(MainActivity.this, "删除成功", 1000);
+                                    }
+                                }, 2000);
+                            }
+                        }))
+                        .show(MainActivity.this);
+            }
+        });
+
+        btn_information.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LemonHello.getInformationHello("您确定要注销吗？", "注销登录后您将无法接收到当前用户的所有推送消息。")
+                        .addAction(new LemonHelloAction("取消", new LemonHelloActionDelegate() {
+                            @Override
+                            public void onClick(LemonHelloView helloView, LemonHelloInfo helloInfo, LemonHelloAction helloAction) {
+                                helloView.hide();
+                            }
+                        }))
+                        .addAction(new LemonHelloAction("我要注销", Color.RED, new LemonHelloActionDelegate() {
+                            @Override
+                            public void onClick(LemonHelloView helloView, LemonHelloInfo helloInfo, LemonHelloAction helloAction) {
+                                helloView.hide();
+                                // 提示框使用了LemonBubble，请您参考：https://github.com/1em0nsOft/LemonBubble4Android
+                                LemonBubble.getRoundProgressBubbleInfo()
+                                        .setLocationStyle(LemonBubbleLocationStyle.BOTTOM)
+                                        .setLayoutStyle(LemonBubbleLayoutStyle.ICON_LEFT_TITLE_RIGHT)
+                                        .setBubbleSize(140, 50)
+                                        .setProportionOfDeviation(0.1f)
+                                        .setTitle("正在请求服务器..")
+                                        .show(MainActivity.this);
+                                new Handler().postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        LemonBubble.showRight(MainActivity.this, "注销成功，欢迎您下次登录", 2000);
+                                    }
+                                }, 1500);
+                            }
+                        }))
+                        .show(MainActivity.this);
+            }
+        });
+        btn_bookmark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LemonHelloInfo bookMarkInfo = new LemonHelloInfo()
+                        .setTitle("添加书签")
+                        .setContent("确认将《LemonKit》添加到您的书签当中吗？")
+                        .setIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.icon_bookmark))
+                        .addAction(new LemonHelloAction("取消", Color.RED, new LemonHelloActionDelegate() {
+                            @Override
+                            public void onClick(LemonHelloView helloView, LemonHelloInfo helloInfo, LemonHelloAction helloAction) {
+                                helloView.hide();
+                            }
+                        }))
+                        .addAction(new LemonHelloAction("我再想想", new LemonHelloActionDelegate() {
+                            @Override
+                            public void onClick(LemonHelloView helloView, LemonHelloInfo helloInfo, LemonHelloAction helloAction) {
+                                helloView.hide();
+                            }
+                        }))
+                        .addAction(new LemonHelloAction("添加", new LemonHelloActionDelegate() {
+                            @Override
+                            public void onClick(LemonHelloView helloView, LemonHelloInfo helloInfo, LemonHelloAction helloAction) {
+                                helloView.hide();
+                                LemonBubble.showRight(MainActivity.this, "添加成功", 1500);
+                            }
+                        }));
+                bookMarkInfo.show(MainActivity.this);
+            }
+        });
+
+        btn_multiMessages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LemonHelloInfo info1 = new LemonHelloInfo()
+                        .setTitle("LemonKit想要获取您的位置")
+                        .setContent("LemonKit获取到您的位置之后将会动态记录您的地理信息。")
+                        .addAction(new LemonHelloAction("允许", new LemonHelloActionDelegate() {
+                            @Override
+                            public void onClick(LemonHelloView helloView, LemonHelloInfo helloInfo, LemonHelloAction helloAction) {
+                                helloView.hide();
+                            }
+                        }))
+                        .addAction(new LemonHelloAction("拒绝", Color.RED, new LemonHelloActionDelegate() {
+                            @Override
+                            public void onClick(LemonHelloView helloView, LemonHelloInfo helloInfo, LemonHelloAction helloAction) {
+                                helloView.hide();
+                            }
+                        }));
+                LemonHelloInfo info2 = new LemonHelloInfo()
+                        .setTitle("LemonKit想要访问数据")
+                        .setContent("LemonKit希望使用蜂窝网络或者WLAN进行远程数据获取。")
+                        .addAction(new LemonHelloAction("允许", new LemonHelloActionDelegate() {
+                            @Override
+                            public void onClick(LemonHelloView helloView, LemonHelloInfo helloInfo, LemonHelloAction helloAction) {
+                                helloView.hide();
+                            }
+                        }))
+                        .addAction(new LemonHelloAction("拒绝", Color.RED, new LemonHelloActionDelegate() {
+                            @Override
+                            public void onClick(LemonHelloView helloView, LemonHelloInfo helloInfo, LemonHelloAction helloAction) {
+                                helloView.hide();
+                            }
+                        }));
+                LemonHelloInfo info3 = new LemonHelloInfo()
+                        .setTitle("LemonKit想要推送通知")
+                        .setContent("LemonKit将要获取通知权限，在适当的时候会向您推送一些通知。")
+                        .addAction(new LemonHelloAction("允许", new LemonHelloActionDelegate() {
+                            @Override
+                            public void onClick(LemonHelloView helloView, LemonHelloInfo helloInfo, LemonHelloAction helloAction) {
+                                helloView.hide();
+                            }
+                        }))
+                        .addAction(new LemonHelloAction("拒绝", Color.RED, new LemonHelloActionDelegate() {
+                            @Override
+                            public void onClick(LemonHelloView helloView, LemonHelloInfo helloInfo, LemonHelloAction helloAction) {
+                                helloView.hide();
+                            }
+                        }));
+                info1.show(MainActivity.this);
+                info2.show(MainActivity.this);
+                info3.show(MainActivity.this);
+            }
+        });
 
     }
 }
